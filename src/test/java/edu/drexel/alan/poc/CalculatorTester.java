c41271901fd7400218fc1665a5ed7aef966d4e42
user <user@users-mbp.hsd1.nj.comcast.net>
2019-06-02 18:18:18 -0400

    Example of locktester

--- src/test/java/edu/drexel/alan/poc/CalculatorTester.java
+++ src/test/java/edu/drexel/alan/poc/CalculatorTester.java
@@ -5,15 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorTester {
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("Setup called");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("Teardown called");
 	}
 
 	@Test
